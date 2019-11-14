ae90549cb0f915c79d4f2c29ef5448984ef93676
Yamboy1 <yamboyd1@gmail.com>
2019-11-14 15:35:29 +1300

    Add travel and get position commands

--- src/model/snake.rs
+++ src/model/snake.rs
@@ -18,6 +18,38 @@ impl Snake {
 	pub fn new(position: Position, direction: Direction) -> Self {
 		Snake { position, direction }
 	}
+
+	/// Moves the Snake one unit in self.direction
+	///
+	/// # Example
+	/// ```
+	/// use snake::{ model::Snake, Direction };
+	///
+	/// let mut snake = Snake::new((0,0), Direction::Right);
+	/// snake.travel();
+	/// println!("{:?}", snake.get_position()); // (-1, 0)
+	/// ```
+	pub fn travel(&mut self) {
+		self.position = match self.direction {
+			Direction::Up => (self.position.0, self.position.1-1),
+			Direction::Down => (self.position.0, self.position.1+1),
+			Direction::Left => (self.position.0+1, self.position.1),
+			Direction::Right => (self.position.0-1, self.position.1),
+		};
+	}
+
+	/// Gets the Snake's current position
+	///
+	/// # Example
+	/// ```
+	/// use snake::{ model::Snake, Direction };
+	///
+	/// let snake = Snake::new((0,0), Direction::Right);
+	/// println!("{:?}", snake.get_position()); // (-1, 0)
+	/// ```
+	pub fn get_position(&self) -> Position {
+		self.position
+	}
 }
 
 #[cfg(test)]
@@ -31,6 +63,13 @@ mod test {
 	}
 	#[test]
 	fn position_can_be_negative() {
-		let snake = Snake::new((-1, -1), Direction::Right);
+		let snake = Snake::new((-1,-1), Direction::Right);
+		assert_eq!(snake.position, (-1,-1))
+	}
+	#[test]
+	fn snake_travels_in_the_right_direction() {
+		let mut snake = Snake::new((0, 0), Direction::Right);
+		snake.travel();
+		assert_eq!(snake.position, (-1,0));
 	}
 }
